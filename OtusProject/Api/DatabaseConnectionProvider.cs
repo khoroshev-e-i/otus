e39b80ec9bd1d5b67e9046d38855a76a9556b086
Евгений Хорошев <khoroshev.e.i@gmail.com>
2024-08-07 20:35:58 +0300

    dz1

---
+++ OtusProject/Api/DatabaseConnectionProvider.cs
@@ -0,0 +1,12 @@
+﻿using Npgsql;
+
+namespace Api;
+
+public class DatabaseConnectionProvider(IConfiguration _configuration)
+{
+    public NpgsqlConnection CreateConnection()
+    {
+        var connectionString = _configuration.GetConnectionString("Postgres");
+        return new NpgsqlConnection(connectionString);
+    }
+}
